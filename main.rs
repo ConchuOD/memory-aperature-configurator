5b75ce5d10228a3c4935c4a27a4649d0f15e58bb
Conor Dooley <mail@conchuod.ie>
2022-05-19 19:12:09 +0100

    add hw_addr -> seg conversion and vice versa
    Signed-off-by: Conor Dooley <mail@conchuod.ie>

--- main.rs
+++ main.rs
@@ -13,17 +13,12 @@ struct MemoryAperture {
 	bus_addr: u64,
 	hardware_addr: u64,
 	aperture_size: u64,
-	seg_reg: u64,
 	reg_name: String
 }
 
 impl Aperture for MemoryAperture {
 
 	fn get_hw_start_addr(&self) -> u64 {
-	// the last hardware addr decided by whichever is lower:
-	// - the addr of the "highest" physical memory on the system
-	// - the and of the aperture into memory on this part of the bus
-
 		self.hardware_addr
 	}
 
@@ -63,9 +58,8 @@ impl Default for MPFS {
 					description: "64-bit cached\t".to_string(),
 					reg_name: "seg0_1".to_string(),
 					bus_addr: 0x10_0000_0000,
-					hardware_addr: 0x0,
+					hardware_addr: 0x00_0200_0000,
 					aperture_size: 0x40_000_0000,
-					seg_reg: 0x0
 				},
 				MemoryAperture {
 					description: "64-bit non-cached".to_string(),
@@ -73,7 +67,6 @@ impl Default for MPFS {
 					bus_addr: 0x14_0000_0000,
 					hardware_addr: 0x0,
 					aperture_size: 0x4000_0000,
-					seg_reg: 0x0
 				},
 				MemoryAperture {
 					description: "64-bit WCB\t".to_string(),
@@ -81,7 +74,6 @@ impl Default for MPFS {
 					bus_addr: 0x18_0000_0000,
 					hardware_addr: 0x0,
 					aperture_size: 0x4000_0000,
-					seg_reg: 0x0
 				},
 				MemoryAperture {
 					description: "32-bit cached\t".to_string(),
@@ -89,7 +81,6 @@ impl Default for MPFS {
 					bus_addr: 0x8000_0000,
 					hardware_addr: 0x0,
 					aperture_size: 0x4000_0000,
-					seg_reg: 0x0
 				},
 				MemoryAperture {
 					description: "32-bit non-cached".to_string(),
@@ -97,7 +88,6 @@ impl Default for MPFS {
 					bus_addr: 0xC000_0000,
 					hardware_addr: 0x0,
 					aperture_size: 0x1000_0000,
-					seg_reg: 0x0
 				},
 				MemoryAperture {
 					description: "32-bit WCB\t".to_string(),
@@ -105,11 +95,42 @@ impl Default for MPFS {
 					bus_addr: 0xD000_0000,
 					hardware_addr: 0x0,
 					aperture_size: 0x1000_0000,
-					seg_reg: 0x0
 				}
 			]
 		}
 	}
+}
+
+fn seg_to_hw_start_addr(seg: u64, bus_addr: u64) -> u64
+{
+	let mut temp = seg;
+
+	if (temp & 0x4000) == 0 {
+	// if that bit isnt set, either this seg register is:
+	// - 0x0 (in which case the hw addr == the bus addr)
+	// - invalid (so treat as zero to match the bootloader's behaviour)
+		return bus_addr
+	}
+
+	temp &= 0x3FFF;
+	temp = 0x4000 - temp;
+	temp <<= 24;
+	bus_addr - temp
+}
+
+fn hw_start_addr_to_seg(hw_start_addr: u64, bus_addr: u64) -> u64
+{
+	if bus_addr == hw_start_addr {
+	// a seg register is effectively how much we need to subtract from the
+	// bus addr to get the hw addr (not /quite/, but sorta) so if they're
+	// the same, then the seg register is 0x0
+		return 0x0
+	}
+
+	let mut temp = bus_addr;
+	temp -= hw_start_addr;
+	temp >>= 24;
+	(0x4000 - temp) | 0x4000
 }
 
 fn hex_to_mib(hex: u64) -> u64
@@ -123,15 +144,10 @@ fn format_segs(memory_apertures: Vec<MemoryAperture>)
 	for memory_aperture in memory_apertures {
 		print!("{}: {:#x?}, ",
 		       memory_aperture.reg_name,
-		       memory_aperture.seg_reg
+		       hw_start_addr_to_seg(memory_aperture.hardware_addr, memory_aperture.bus_addr)
 		);
 	}
 	print!("}}\n");
-}
-
-fn seg_to_hw_start_addr(seg: u64) -> u64
-{
-	1_u64
 }
 
 fn main() {
@@ -145,8 +161,11 @@ fn main() {
 			 memory_aperture.bus_addr,
 			 memory_aperture.get_hw_start_addr(),
 			 memory_aperture.get_hw_end_addr(board.total_system_memory),
-			 hex_to_mib(memory_aperture.get_hw_end_addr(board.total_system_memory) - memory_aperture.get_hw_start_addr())
+			 hex_to_mib(memory_aperture.get_hw_end_addr(board.total_system_memory)
+			 	    - memory_aperture.get_hw_start_addr())
 		);
 	}
 	format_segs(board.memory_apertures);
+	println!("{:#012x?}\n", seg_to_hw_start_addr(0x7002, 0x10_0000_0000));
+	println!("{:#012x?}\n", hw_start_addr_to_seg(seg_to_hw_start_addr(0x7002, 0x10_0000_0000), 0x10_0000_0000));
 }
